6a517caa8628edd4830703f690cdb810b7244eff
joshuaatencia <joshuaedts@gmail.com>
2021-03-15 10:39:03 -0500

    authentication google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require("../models/usuarios-model");
 var bcrypt = require("bcryptjs");
 const { generarJWT } = require("../helper/jwt");
+const { googleVerify } = require("../helper/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -28,12 +29,10 @@ const login = async (req, res = response) => {
     // generar token
     const token = await generarJWT(usuarioDB.id);
 
-
     res.json({
-        ok:true,
-        token
+      ok: true,
+      token,
     });
-
   } catch (error) {
     console.log(error);
     res.json({
@@ -43,4 +42,45 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  const googleToken = req.body.token;
+
+  try {
+    const { name, email, picture } = await googleVerify(googleToken);
+
+    const usuarioDB = await Usuario.findOne({ email });
+
+    let usuario;
+
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: '@@@',
+        img:picture,
+        google: true
+      });
+    }else{
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    //guardar en bd
+    await usuario.save();
+
+    // generar token
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      token
+    });
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      msg: "Token no es correcto",
+    });
+  }
+};
+
+module.exports = { login, googleSignIn };
